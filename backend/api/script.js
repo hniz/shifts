e283d0aa8af41c04f2115ae685e9db234a88e878
Eric Altenburg <ealtenbu@stevens.edu>
2021-05-15 02:53:02 -0400

    Added worker for scraper API (partial)

--- backend/api/script.js
+++ backend/api/script.js
@@ -1,13 +1,13 @@
-// const scraper = require('./app');
+const scraper = require('./app');
 
-// setInterval( async () => {
-//     let results = null;
-//     results = await scraper.scrapeReddit(["wallstreetbets", "stocks", "investing", "pennystocks"], 5);
-//     results = results[0]
-//     results = results.sort((a,b) => {
-//         return b.timesCounted - a.timesCounted;
-//     });
-// }, 180000);
+setInterval( async () => {
+    let results = null;
+    results = await scraper.scrapeReddit(["wallstreetbets", "stocks", "investing", "pennystocks"], 5);
+    results = results[0]
+    results = results.sort((a,b) => {
+        return b.timesCounted - a.timesCounted;
+    });
+}, 180000);
 
 // on interval of 12 or 24 hours run:
     // call hamzah's scraper, get the results push to db
